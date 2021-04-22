9800b7615fa8babe64c1a2620896bd4f8d9cd85d
ashi <zma4580@gmail.com>
2021-04-21 22:19:12 -0400

    Here we go

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Response, Request
+import requests
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import HTMLResponse
 
-from app.core.config import settings
+app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=10)
 
-
-def get_application():
-    _app = FastAPI(title=settings.PROJECT_NAME)
-
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    return _app
-
-
-app = get_application()
+@app.get("/simple/{package}/", response_class=HTMLResponse)
+async def pypi(package, request:Request, response:Response):
+    url = 'https://pypi.org/simple/%s/' % package
+    r = requests.get(url)
+    if r.ok:
+        dont_add_headers = [
+            'Content-Length',
+            'Content-Encoding'
+        ]
+        for header_key in r.headers.keys():
+            if header_key not in dont_add_headers:
+                response.headers[header_key] = r.headers[header_key]
+        return r.text
